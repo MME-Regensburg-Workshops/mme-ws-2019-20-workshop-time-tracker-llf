f7347dc091357fa30db5ecaece7e5b3fa5fabe54
Luis Mossburger <luismossburger@github.com>
2020-01-08 13:58:13 +0100

    Grundstruktur

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,23 @@
 /* eslint-env browser */
 
+//PLAN
+/*
+
+MODEL_____
+TASK.js         Speichert ID, Beschreibung, Tags, (dynamische) Dauer, Abgeschlossen (boolean)
+TASK_LIST.js    Array aus Tasks, Suchfunktion
+TIME.js         Zählt & gibt Dauer der Aufgabe zurück
+
+VIEW_____
+TASK_VIEW.js    Element für einzelnen Task
+CONTROL_VIEW.js Für Pause/Stopp-Funktion pro Task
+                + Hinweis "xx nicht angezeigt"
+FORM_VIEW.js    Formular für Anlegen neuem Task
+
+
+*/
+
+
 function init() {
 }
 
